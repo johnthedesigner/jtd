c3668c24b1d794cc8b71f4686b4c9b876fd34cf4
johnthedesigner <john@johnthedesigner.com>
2023-02-11 12:36:51 -0500

    fix logo shuffle for mobile devices

--- components/SketchLogo.js
+++ components/SketchLogo.js
@@ -1,104 +1,110 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const SketchLogo = () => {
-    const jArray = ['Cookie', 'Dog', 'Flamingo', 'Lego', 'Sneaker']
-    const tArray = [
-        'Birdhouse',
-        'Groucho',
-        'MushroomCloud',
-        'Music',
-        'Skull',
-        'Spaceman',
-        'Umbrella',
-    ]
-    const dArray = ['B-Ball', 'Bowling', 'Burger', 'Island', 'Mug', 'Pencil']
+    // New logo state
+    const jLength = 5
+    const tLength = 8
+    const dLength = 6
+    const [jIndex, setJindex] = useState(0)
+    const [tIndex, setTindex] = useState(0)
+    const [dIndex, setDindex] = useState(0)
 
-    const [j, setJ] = useState('Cookie')
-    const [t, setT] = useState('Groucho')
-    const [d, setD] = useState('Pencil')
+    const randomizeLetter = () => {
+        let indices = [jIndex, tIndex, dIndex]
+        let lengths = [jLength, tLength, dLength]
+        let setters = [setJindex, setTindex, setDindex]
 
-    const randomizeSketches = async () => {
-        let sketches = [j, t, d]
-        let setters = [setJ, setT, setD]
-        let arrays = [jArray, tArray, dArray]
-
+        // Choose which letter to randomize
         let selectedLetter = Math.floor(Math.random() * 3)
 
+        // Choose a random index based on number of options for selected letter
         let selectedSketchIndex = Math.floor(
-            Math.random() * arrays[selectedLetter].length
+            Math.random() * lengths[selectedLetter]
         )
-        let selectedSketch = arrays[selectedLetter][selectedSketchIndex]
 
-        if (selectedSketch === sketches[selectedLetter]) {
-            if (selectedSketchIndex + 1 === arrays[selectedLetter].length) {
-                selectedSketch = arrays[selectedLetter][selectedSketchIndex - 1]
+        // Check if we chose the same letter
+        if (selectedSketchIndex === indices[selectedLetter]) {
+            // If we're at the end of the list, decrement otherwise increment
+            if (selectedSketchIndex + 1 === lengths[selectedLetter]) {
+                selectedSketchIndex--
             } else {
-                selectedSketch = arrays[selectedLetter][selectedSketchIndex + 1]
+                selectedSketchIndex++
             }
         }
 
-        await setters[selectedLetter](selectedSketch)
+        // Set selected letter to new index
+        setters[selectedLetter](selectedSketchIndex)
     }
+
+    // Old Logo state
+    // const jArray = ['Cookie', 'Dog', 'Flamingo', 'Lego', 'Sneaker']
+    // const tArray = [
+    //     'Birdhouse',
+    //     'Groucho',
+    //     'MushroomCloud',
+    //     'Music',
+    //     'Screw',
+    //     'Skull',
+    //     'Spaceman',
+    //     'Umbrella',
+    // ]
+    // const dArray = ['B-Ball', 'Bowling', 'Burger', 'Island', 'Mug', 'Pencil']
+
+    // const [j, setJ] = useState('Cookie')
+    // const [t, setT] = useState('Groucho')
+    // const [d, setD] = useState('Pencil')
+
+    // const randomizeSketches = async () => {
+    //     let sketches = [j, t, d]
+    //     let setters = [setJ, setT, setD]
+    //     let arrays = [jArray, tArray, dArray]
+
+    //     let selectedLetter = Math.floor(Math.random() * 3)
+
+    //     let selectedSketchIndex = Math.floor(
+    //         Math.random() * arrays[selectedLetter].length
+    //     )
+    //     let selectedSketch = arrays[selectedLetter][selectedSketchIndex]
+
+    //     if (selectedSketch === sketches[selectedLetter]) {
+    //         if (selectedSketchIndex + 1 === arrays[selectedLetter].length) {
+    //             selectedSketch = arrays[selectedLetter][selectedSketchIndex - 1]
+    //         } else {
+    //             selectedSketch = arrays[selectedLetter][selectedSketchIndex + 1]
+    //         }
+    //     }
+
+    //     await process.nextTick(() => {
+    //         setters[selectedLetter](selectedSketch)
+    //     })
+    // }
 
     // Loop through random sketches
     let speed = 12
     let iterations = 27
     var step = 0
     const triggerRandomize = () => {
-        randomizeSketches()
+        randomizeLetter()
         if (step < iterations) {
             step++
             setTimeout(triggerRandomize, speed * step ** 1.03)
         }
     }
-
-    useState(() => {
+    useEffect(() => {
         triggerRandomize()
-    })
-
-    const sketchLetterStyles = {
-        width: '100%',
-        height: 'auto',
-        position: 'absolute',
-        pointerEvents: 'none',
-    }
-
-    const SketchLetter = ({ nameState, letter, name }) => {
-        let hidden = nameState != name
-        return (
-            <Image
-                src={`/${letter}/${name}.png`}
-                className="sketch-logo__sketch-letter"
-                width="520"
-                height="230"
-                style={{
-                    ...sketchLetterStyles,
-                    opacity: hidden ? 0 : 1,
-                }}
-                alt="Sketched letter overlay"
-            />
-        )
-    }
+    }, [])
 
     return (
-        <div
-            className="sketch-logo"
-            style={{
-                position: 'relative',
-                width: '100%',
-                aspectRatio: '520/230',
-            }}
-            onClick={triggerRandomize}
-        >
+        <div className="sketch-logo" onClick={triggerRandomize}>
             <div className="sketch-logo__letters">
                 <svg
-                    width="520"
-                    height="230"
+                    className="sketch-logo__letters-backdrop"
+                    width="20em"
+                    // height="8.85em"
                     viewBox="0 0 520 230"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
-                    style={sketchLetterStyles}
                 >
                     <path
                         d="M190.007 65.1805C189.563 62.6726 191.243 60.281 193.758 59.8388L314.723 38.5708C317.238 38.1286 319.637 39.8032 320.08 42.3111L329.942 98.0785C330.385 100.586 328.706 102.978 326.191 103.42L300.504 107.936C299.246 108.158 298.406 109.353 298.628 110.607L308.891 168.645C309.335 171.153 307.655 173.545 305.14 173.987L249.212 183.82C246.696 184.262 244.298 182.588 243.855 180.08L233.591 122.042C233.37 120.788 232.17 119.951 230.913 120.172L205.226 124.688C202.71 125.13 200.312 123.456 199.868 120.948L190.007 65.1805Z"
@@ -113,28 +119,163 @@ const SketchLogo = () => {
                         fill="#5F9DF2"
                     />
                 </svg>
-                <SketchLetter nameState={j} letter="j" name="Baby" />
-                <SketchLetter nameState={j} letter="j" name="Cookie" />
-                <SketchLetter nameState={j} letter="j" name="Dog" />
-                <SketchLetter nameState={j} letter="j" name="Flamingo" />
-                <SketchLetter nameState={j} letter="j" name="Lego" />
-                <SketchLetter nameState={j} letter="j" name="Sneaker" />
-                <SketchLetter nameState={t} letter="t" name="Birdhouse" />
-                <SketchLetter nameState={t} letter="t" name="Groucho" />
-                <SketchLetter nameState={t} letter="t" name="MushroomCloud" />
-                <SketchLetter nameState={t} letter="t" name="Music" />
-                <SketchLetter nameState={t} letter="t" name="Pizza" />
-                <SketchLetter nameState={t} letter="t" name="Screw" />
-                <SketchLetter nameState={t} letter="t" name="Skull" />
-                <SketchLetter nameState={t} letter="t" name="Spaceman" />
-                <SketchLetter nameState={t} letter="t" name="Umbrella" />
-                <SketchLetter nameState={d} letter="d" name="B-Ball" />
-                <SketchLetter nameState={d} letter="d" name="Bowling" />
-                <SketchLetter nameState={d} letter="d" name="Burger" />
-                <SketchLetter nameState={d} letter="d" name="Island" />
-                <SketchLetter nameState={d} letter="d" name="Mitt" />
-                <SketchLetter nameState={d} letter="d" name="Mug" />
-                <SketchLetter nameState={d} letter="d" name="Pencil" />
+                <div
+                    className="sketch-logo__letter-stack"
+                    style={{
+                        marginLeft: `${-100 * jIndex}%`,
+                        width: `${100 * jLength}%`,
+                    }}
+                >
+                    <Image
+                        src={`/j/Cookie.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/j/Dog.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/j/Flamingo.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/j/Lego.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/j/Sneaker.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                </div>
+                <div
+                    className="sketch-logo__letter-stack"
+                    style={{
+                        marginLeft: `${-100 * tIndex}%`,
+                        width: `${100 * tLength}%`,
+                    }}
+                >
+                    <Image
+                        src={`/t/Birdhouse.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/t/Groucho.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/t/MushroomCloud.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/t/Music.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/t/Screw.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/t/Skull.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/t/Spaceman.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/t/Umbrella.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                </div>
+                <div
+                    className="sketch-logo__letter-stack"
+                    style={{
+                        marginLeft: `${-100 * dIndex}%`,
+                        width: `${100 * dLength}%`,
+                    }}
+                >
+                    <Image
+                        src={`/d/B-Ball.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/d/Bowling.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/d/Burger.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/d/Island.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/d/Mug.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                    <Image
+                        src={`/d/Pencil.png`}
+                        className="sketch-logo__sketch-letter"
+                        width="520"
+                        height="230"
+                        alt="Sketched letter overlay"
+                    />
+                </div>
             </div>
         </div>
     )
